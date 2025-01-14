9eddf6792a7990ee170c892a854a46ae79d4e09d
hao.zhang <>
2025-01-14 16:46:54 +0800

    添加 sonar测试漏洞

---
+++ src/main/java/com/example/demo/HardcodedPassword.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class HardcodedPassword {
+    public static void main(String[] args) {
+        String password = "mySecretPassword123";  // 硬编码密码
+        System.out.println("Password: " + password);
+    }
+}
