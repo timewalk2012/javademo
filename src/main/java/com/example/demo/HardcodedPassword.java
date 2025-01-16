c86112122b21b8d32ddbaaa421f0382c92e6ee14
hao.zhang <>
2025-01-16 15:29:51 +0800

    删除测试故障类

--- src/main/java/com/example/demo/HardcodedPassword.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class HardcodedPassword {
-    public static void main(String[] args) {
-        String password = "mySecretPassword123";  // 硬编码密码
-        System.out.println("Password: " + password);
-    }
-}
